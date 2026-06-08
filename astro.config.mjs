e735c2b9f5e5489e753e13a3a30bea0a036d0965
David <davidhidayat.nh11@gmail.com>
2026-06-08 13:49:41 +0700

    update porto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,28 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [icon()],
+
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      ViteImageOptimizer({
+        test: /\.(jpe?g|png|gif|tiff|webp|svg)$/i,
+        webp: {
+          quality: 80,
+        },
+        png: {
+          quality: 80,
+        },
+        jpeg: {
+          quality: 80,
+        },
+      }),
+    ],
   },
 });
